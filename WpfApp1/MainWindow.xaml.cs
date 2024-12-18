e84faee5a5a451e5055cdfb11825b9cd9b5a65f9
student <student@DESKTOP-AAAA14>
2024-12-18 15:24:39 +0200

    end

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,24 +21,56 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Button btn1, btn2;
+        int moves = 0, cardsshow=0, count=4;
+
+        private void to_start_the_game_Click(object sender, RoutedEventArgs e)
+        {
+            Button start = sender as Button;
+            to_start_the_game.Visibility = Visibility.Collapsed;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-
-        }
-        public void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Button button = (Button)sender;
+ 
             
-            // button.Visibility = Visibility.Hidden;
-
-            DoubleAnimation widthAnimation = new DoubleAnimation
+        }
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            moves++;
+            btn.Visibility = Visibility.Collapsed;//לחשוף את הקךף שנלחץ
+            if (btn1 == null)
             {
-                From = button.ActualWidth,
-                To = 0,
-                Duration = new Duration(TimeSpan.FromSeconds(3)),
-            };
-            button.BeginAnimation(Button.WidthProperty, widthAnimation);
+                btn1 = btn;//save the first card
+            }
+            else
+            {
+                if(btn2 == null)// chack if it is the second card
+                {
+                    btn2 = btn;//save the second card
+                    if (count == cardsshow + 1)//the game is over?
+                    {
+                        MessageBox.Show($"כל הכבוד נצחת ב {moves} מהלכים!!!!!!");
+                    }
+                }
+                else
+                {
+
+                    if (btn1.Tag.ToString()==btn2.Tag.ToString())
+                    {
+                        cardsshow += 1;
+                    }
+                    else
+                    {
+                        btn1.Visibility = Visibility.Visible;
+                        btn2.Visibility = Visibility.Visible;
+                    } 
+                    btn1 = btn;
+                    btn2 = null;//we don't heve scond card
+                }
+            }
         }
 
 
